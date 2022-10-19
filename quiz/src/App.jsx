bfabeb7ea74c5b6ed4640891c09503ff5e7d65f3
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-19 10:14:59 -0300

    gameover component

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -3,6 +3,7 @@ import { QuizContext } from './context/quiz';
 
 import Welcome from './components/Welcome';
 import Question from './components/Question';
+import GameOver from './components/GameOver';
 
 import './App.css';
 
@@ -19,6 +20,7 @@ const App = () => {
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === 'Start' && <Welcome />}
       {quizState.gameStage === 'Playing' && <Question />}
+      {quizState.gameStage === 'End' && <GameOver />}
     </>
   );
 };
