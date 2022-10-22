3555041e2b0c7adb33cbb991b7066d8aaca676a9
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-22 10:24:03 -0300

    first commit

---
+++ movies/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// css
+import './App.scss'
+
+const App = () => {
+  return (
+    <>
+      <h1>hello world!</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ movies/src/pages/Movie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Movie = (props) => {
+  return (
+    <div>
+      <h1>movie</h1>
+    </div>
+  )
+}
+
+export default Movie
