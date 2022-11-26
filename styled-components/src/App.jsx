6d8f709ec49025324258ac057c0b4da1311f89b2
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-26 15:20:34 -0300

    add theme, header and global style to app

--- styled-components/src/App.jsx
+++ styled-components/src/App.jsx
@@ -1,10 +1,27 @@
+import { ThemeProvider } from 'styled-components';
 import { Container } from './components/styles/Container.styled';
+import GlobalStyles from './components/styles/Global';
+import Header from './components/Header';
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+};
 
 const App = () => {
   return (
-    <Container>
-      <h1>hello world!</h1>
-    </Container>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          <h1>hello world!</h1>
+        </Container>
+      </ThemeProvider>
+    </>
   );
 };
 
