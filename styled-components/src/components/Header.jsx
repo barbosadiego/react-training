e5eee067fb312771b8913a9bca824badbd3eb8b5
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-26 15:38:06 -0300

    add nav, button and logo to header

--- styled-components/src/components/Header.jsx
+++ styled-components/src/components/Header.jsx
@@ -1,9 +1,16 @@
-import { StyledHeader } from './styles/Header.styled';
+import { StyledHeader, Nav, Logo } from './styles/Header.styled';
+import { Container } from './styles/Container.styled';
+import Button from './styles/Button.styled';
 
 const Header = () => {
   return (
     <StyledHeader>
-      <h1>header</h1>
+      <Container>
+        <Nav>
+          <Logo src="./assets/logo.svg" alt="logo" />
+          <Button>Try it free</Button>
+        </Nav>
+      </Container>
     </StyledHeader>
   );
 };
