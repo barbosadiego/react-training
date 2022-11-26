2849a2ef1380d536e1cca73fa3004846d14dce08
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-26 16:05:42 -0300

    add flex and image components to header

--- styled-components/src/components/Header.jsx
+++ styled-components/src/components/Header.jsx
@@ -1,5 +1,6 @@
-import { StyledHeader, Nav, Logo } from './styles/Header.styled';
+import { StyledHeader, Nav, Logo, Image } from './styles/Header.styled';
 import { Container } from './styles/Container.styled';
+import Flex from './styles/Flex.styled';
 import Button from './styles/Button.styled';
 
 const Header = () => {
@@ -10,6 +11,21 @@ const Header = () => {
           <Logo src="./assets/logo.svg" alt="logo" />
           <Button>Try it free</Button>
         </Nav>
+        <Flex>
+          <div>
+            <h1>Build The Community Your Fans Will Love</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+              dolor illo fugiat maiores consequatur. Rem tenetur quaerat
+              consectetur tempore sed, atque repellendus commodi quia tempora
+              totam hic nihil facilis ipsa.
+            </p>
+            <Button bg="#ff0099" color="#fff">
+              Get Started For Free
+            </Button>
+          </div>
+          <Image src="./assets/illustration-mockups.svg" alt="" />
+        </Flex>
       </Container>
     </StyledHeader>
   );
